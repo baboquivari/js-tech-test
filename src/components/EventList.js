d78e4c0456c7d841393d54212a843b6dcd510875
unknown <to.sian.san@gmail.com>
2018-02-24 14:49:21 +0000

    initial setup

---
+++ src/components/EventList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class EventList extends Component {
+    render () {
+        return (
+            <div>EventList wooo</div>
+        );
+    }
+}
+
+export default EventList;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import EventList from './components/EventList';
+
+ReactDOM.render(
+    <EventList />,
+    document.getElementById('app')
+);
